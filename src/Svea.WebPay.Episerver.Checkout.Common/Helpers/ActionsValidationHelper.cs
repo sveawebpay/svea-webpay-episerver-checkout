336dd82c4ac679c731ed3e38e4c08f579887eea7
Daniel Sundqvist <daniel.sundqvist@authority.se>
2021-03-12 08:35:03 +0100

    Added ActionsValidationHelper

---
+++ src/Svea.WebPay.Episerver.Checkout.Common/Helpers/ActionsValidationHelper.cs
@@ -0,0 +1,86 @@
+﻿using System.Linq;
+using Svea.WebPay.SDK.PaymentAdminApi.Models;
+
+namespace Svea.WebPay.Episerver.Checkout.Common.Helpers
+{
+	public static class ActionsValidationHelper
+    {
+	    public static string ValidateOrderAction(Order order, string orderAction)
+	    {
+		    if (order == null)
+		    {
+			    return "Payment Order does not exist";
+		    }
+
+		    if (orderAction == null)
+		    {
+			    return null;
+		    }
+
+		    if (!order.AvailableActions.Contains(orderAction))
+		    {
+			    return "Operation not available";
+		    }
+
+		    return null;
+	    }
+
+	    public static string ValidateOrderRowAction(Order order, long orderRowId, string orderRowAction)
+	    {
+		    var orderError = ValidateOrderAction(order, null);
+
+		    if (orderError != null)
+		    {
+			    return orderError;
+		    }
+
+		    var orderRow = order.OrderRows.FirstOrDefault(row => row.OrderRowId == orderRowId);
+
+		    if (orderRow == null)
+		    {
+			    return "Order row does not exist";
+		    }
+
+		    if (orderRowAction == null)
+		    {
+			    return null;
+		    }
+
+		    if (!orderRow.AvailableActions.Contains(orderRowAction))
+		    {
+			    return "Operation not available";
+		    }
+
+		    return null;
+	    }
+
+	    public static string ValidateDeliveryAction(Order order, long deliveryId, string deliveryAction)
+	    {
+		    var orderError = ValidateOrderAction(order, null);
+
+		    if (orderError != null)
+		    {
+			    return orderError;
+		    }
+
+		    var delivery = order.Deliveries.FirstOrDefault(dlv => dlv.Id == deliveryId);
+
+		    if (delivery == null)
+		    {
+			    return "Order row does not exist";
+		    }
+
+		    if (deliveryAction == null)
+		    {
+			    return null;
+		    }
+
+		    if (!delivery.AvailableActions.Contains(deliveryAction))
+		    {
+			    return "Operation not available";
+		    }
+
+		    return null;
+	    }
+	}
+}
